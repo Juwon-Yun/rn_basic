456b9a79f491251693af5e866cd833399f9612e2
Juwon-Yun <eooll2@daum.net>
2022-08-06 13:07:10 +0900

    feature: rn layout, dimensions, flex

--- src/screen/layout_screen.tsx
+++ src/screen/layout_screen.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import {ScrollView, View} from 'react-native';
+import {Dimensions, ScrollView, View} from 'react-native';
 
 const LayoutScreen = () => {
   // View는 기본이 FlexBox다
 
   return (
     <ScrollView>
-      <View style={{flexDirection: 'column'}}>
-        <View style={{width: 200, height: 200, backgroundColor: 'red'}} />
-        <View style={{width: 200, height: 200, backgroundColor: 'black'}} />
-        <View style={{width: 200, height: 200, backgroundColor: 'blue'}} />
-        <View style={{width: 200, height: 200, backgroundColor: 'green'}} />
-        <View style={{width: 200, height: 200, backgroundColor: 'white'}} />
+      <View
+        style={{
+          height: Dimensions.get('screen').height,
+          width: Dimensions.get('screen').width,
+        }}>
+        <View style={{flex: 1, backgroundColor: 'red'}} />
+        <View style={{flex: 1, backgroundColor: 'black'}} />
+        <View style={{flex: 3, backgroundColor: 'blue'}} />
+        <View style={{flex: 1, backgroundColor: 'green'}} />
+        <View style={{flex: 1, backgroundColor: 'white'}} />
       </View>
     </ScrollView>
   );
